5ff2e09cb125ca331b4a96f4906267c0242cef70
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2023-03-16 19:27:26 -0600

    se agrega el proyecto

--- js/scripts.js
+++ js/scripts.js
@@ -1,249 +1,219 @@
-// Código final se aplicaron funciones, constantes, condiciones y ciclos
-function ingresarDatos(mensaje) {
-    const dato = prompt(mensaje);
-    console.log(dato);
-    return dato;
+// El proyecto consiste en simular el proceso de inscripción de alumnos a un Diplomado para certificarse como guía Montessori
+
+// Declaración de variables
+let nombreCompleto;
+let primerNombre;
+let segundoNombre;
+let primerApellido;
+let segundoApellido;
+let edad;
+let correo;
+let telefono;
+let direccion;
+let ciudad;
+let estado;
+let pais;
+let nacionalidad;
+let codigoPostal;
+let fechaNacimiento;
+let fechaInscripcion;
+let fechaInicio;
+let fechaFin;
+let costo;
+let pago;
+let pagoTotal;
+let pagoRestante;
+
+// Capturar el nombre del alumno usando el método trim para eliminar los espacios en blanco
+let nombres = prompt("Ingrese el nombre completo del alumno: ").trim();
+
+// Validar que el usuario haya ingresado al menos un nombre usando el ciclo while
+while (!nombres || nombres.split(" ").length < 1) {
+    nombres = prompt("Debe ingresar al menos un nombre. Ingrese el nombre completo del alumno: ").trim();
 }
 
-let datosCorrectos = false;
-let primerNombre, segundoNombre, apellidoPaterno, apellidoMaterno;
+// Capturar el nombre del alumno usando el método split() y convertirlo para que esté en minúsculas
+let partesNombre = nombres.split(" ");
+primerNombre = partesNombre[0].toLowerCase();
+segundoNombre = partesNombre.length > 1 ? partesNombre[1].toLowerCase() : null;
+primerApellido = partesNombre.length > 2 ? partesNombre[2].toLowerCase() : null;
+segundoApellido = partesNombre.length > 3 ? partesNombre.slice(3).join(" ").toLowerCase() : null;
 
-do {
-    // Pedir los nombres y apellidos y aplicar formato para que solo la primer letra de cada ingreso esté en mayúscula
-    primerNombre = ingresarDatos("Ingresa tu primer nombre");
-    const primeraLetraMayusculaPrimerNombre = primerNombre.charAt(0).toUpperCase();
-    const restoPrimerNombre = primerNombre.slice(1).toLowerCase();
-    primerNombre = primeraLetraMayusculaPrimerNombre + restoPrimerNombre;
+console.log("Nombre completo del alumno:", nombres);
 
-    segundoNombre = ingresarDatos("Ingresa tu segundo nombre");
-    const primeraLetraMayusculaSegundoNombre = segundoNombre.charAt(0).toUpperCase();
-    const restoSegundoNombre = segundoNombre.slice(1).toLowerCase();
-    segundoNombre = primeraLetraMayusculaSegundoNombre + restoSegundoNombre;
+// Capturar la fecha de nacimiento del alumno y validar que lo haya hecho en el formato pedido
+// Se declara la variable en false para iniciar el ciclo while
+let fechaValida = false;
 
-    apellidoPaterno = ingresarDatos("Ingresa tu apellido paterno");
-    const primeraLetraMayusculaApellidoPaterno = apellidoPaterno.charAt(0).toUpperCase();
-    const restoApellidoPaterno = apellidoPaterno.slice(1).toLowerCase();
-    apellidoPaterno = primeraLetraMayusculaApellidoPaterno + restoApellidoPaterno;
-
-    apellidoMaterno = ingresarDatos("Ingresa tu apellido materno");
-    const primeraLetraMayusculaApellidoMaterno = apellidoMaterno.charAt(0).toUpperCase();
-    const restoApellidoMaterno = apellidoMaterno.slice(1).toLowerCase();
-    apellidoMaterno = primeraLetraMayusculaApellidoMaterno + restoApellidoMaterno;
-
-    // Mostrar los datos ingresados
-    const datosIngresados = `Por favor, revisa que los datos ingresados sean correctos:\nTu primer nombre es: ${primerNombre}\nTu segundo nombre es: ${segundoNombre}\nTu apellido paterno es: ${apellidoPaterno}\nTu apellido materno es: ${apellidoMaterno}`;
-    alert(datosIngresados);
-
-    // Preguntar si los datos ingresados son correctos
-    const confirmacion = confirm("¿Los datos ingresados son correctos?");
-
-    if (confirmacion) {
-        datosCorrectos = true;
-    }
-
-} while (!datosCorrectos);
-// Formatear el nombre completo
-const nombreCompleto = `${primerNombre} ${segundoNombre} ${apellidoPaterno} ${apellidoMaterno}`;
-
-// Mostrar el nombre completo con el formato aplicado usando templete literals
-const nombreCompletoMensaje = `Se ha registrado tu nombre completo: "${nombreCompleto}" en nuestra base de datos, gracias por registrarte.`;
-console.log(nombreCompletoMensaje);
-alert(nombreCompletoMensaje);
-
-
-/*
-
-A continuación se muestra la evolución del código en comentarios:
-
---- Primer intento ---
-
-let primerNombre = prompt("Ingresa tu primer nombre");
-console.log(primerNombre);
-let segundoNombre = prompt("Ingresa tu segundo nombre");
-console.log(segundoNombre);
-let apellidoPaterno = prompt("Ingresa tu apellido paterno");
-console.log(apellidoPaterno);
-let apellidoMaterno = prompt("Ingresa tu apellido materno");
-console.log(apellidoMaterno);
-const nombreCompleto = primerNombre + " " + segundoNombre + " " + apellidoPaterno + " " + apellidoMaterno;
-console.log("tu nombre completo es: ", nombreCompleto);
-alert("Tu nombre completo es: ", nombreCompleto);
-*/
-
-/*
---- Segundo intento ---
-
-Se reemplazaron los let por const
-Se usó una función para ingresar los datos en lugar de generar una variable cada vez
-Se aplicó el formato para que solo la primer letra de cada ingreso esté en mayúscula
-Se aplicó el formato para que el nombre completo se muestre con la primer letra de cada palabra en mayúscula
-Por más que intenté no pude hacer que reconozca la Ñ en un nombre y lo muestra asi "MuñOz"
-
-
-// Función para ingresar datos
-function ingresarDatos(mensaje) {
-    const dato = prompt(mensaje);
-    console.log(dato);
-    return dato;
-}
-
-// Pedir los nombres y apellidos y aplicar formato para que solo la primer letra de cada ingreso esté en mayúscula
-const primerNombre = ingresarDatos("Ingresa tu primer nombre");
-const primeraLetraMayusculaPrimerNombre = primerNombre.charAt(0).toUpperCase();
-const restoPrimerNombre = primerNombre.slice(1).toLowerCase();
-const primerNombreMayuscula = primeraLetraMayusculaPrimerNombre + restoPrimerNombre;
-
-const segundoNombre = ingresarDatos("Ingresa tu segundo nombre");
-const primeraLetraMayusculaSegundoNombre = segundoNombre.charAt(0).toUpperCase();
-const restoSegundoNombre = segundoNombre.slice(1).toLowerCase();
-const segundoNombreMayuscula = primeraLetraMayusculaSegundoNombre + restoSegundoNombre;
-
-const apellidoPaterno = ingresarDatos("Ingresa tu apellido paterno");
-const primeraLetraMayusculaApellidoPaterno = apellidoPaterno.charAt(0).toUpperCase();
-const restoApellidoPaterno = apellidoPaterno.slice(1).toLowerCase();
-const apellidoPaternoMayuscula = primeraLetraMayusculaApellidoPaterno + restoApellidoPaterno;
-
-const apellidoMaterno = ingresarDatos("Ingresa tu apellido materno");
-const primeraLetraMayusculaApellidoMaterno = apellidoMaterno.charAt(0).toUpperCase();
-const restoApellidoMaterno = apellidoMaterno.slice(1).toLowerCase();
-const apellidoMaternoMayuscula = primeraLetraMayusculaApellidoMaterno + restoApellidoMaterno;
-
-// Mostrar os datos ingresados con el formato aplicado
-console.log(`Tu primer nombre es: ${primerNombreMayuscula}`);
-console.log(`Tu segundo nombre es: ${segundoNombreMayuscula}`);
-console.log(`Tu apellido paterno es: ${apellidoPaternoMayuscula}`);
-console.log(`Tu apellido materno es: ${apellidoMaternoMayuscula}`);
-
-// Formatear el nombre completo
-const nombreCompleto = `${primerNombreMayuscula} ${segundoNombreMayuscula} ${apellidoPaternoMayuscula} ${apellidoMaternoMayuscula}`;
-const nombreCompletoMayuscilas = nombreCompleto
-    .toLowerCase() // convertir todo a minúsculas
-    .replace(/\b\w/g, (l) => l.toUpperCase()); // reemplazar el primer caracter de cada palabra por mayúscula, por más que intento no puedo corregir que un dato que tenga Ñ se muestre como se espera
-
-// Mostrar el nombre completo con el formato aplicado
-console.log(`Tu nombre completo es: ${nombreCompletoMayuscilas}`);
-alert(`Tu nombre completo es: ${nombreCompletoMayuscilas}`);
-
- */
-
-/*
---- Cuarto Intento ---
-Se generó una función para aplicar las mayúsculas
-
-
-// Función para aplicar el formato de primera letra mayúscula
-function formatoMayusculas(texto) {
-  return texto.charAt(0).toUpperCase() + texto.slice(1).toLowerCase();
-}
-
-// Función para ingresar datos
-function ingresarDatos(mensaje) {
-    const dato = prompt(mensaje);
-    console.log(dato);
-    return dato;
-}
-
-// Pedir los nombres y apellidos y aplicar el formato
-const primerNombre = formatoMayusculas(ingresarDatos("Ingresa tu primer nombre"));
-const segundoNombre = formatoMayusculas(ingresarDatos("Ingresa tu segundo nombre"));
-const apellidoPaterno = formatoMayusculas(ingresarDatos("Ingresa tu apellido paterno"));
-const apellidoMaterno = formatoMayusculas(ingresarDatos("Ingresa tu apellido materno"));
-
-// Mostrar los datos ingresados con el formato aplicado
-console.log(`Tu primer nombre es: ${primerNombre}`);
-console.log(`Tu segundo nombre es: ${segundoNombre}`);
-console.log(`Tu apellido paterno es: ${apellidoPaterno}`);
-console.log(`Tu apellido materno es: ${apellidoMaterno}`);
-
-// Formatear el nombre completo
-const nombreCompleto = `${primerNombre} ${segundoNombre} ${apellidoPaterno} ${apellidoMaterno}`;
-const nombreCompletoMayusculas = nombreCompleto.replace(/\b\w/g, (l) => l.toUpperCase());
-
-// Mostrar el nombre completo con el formato aplicado
-console.log(`Tu nombre completo es: ${nombreCompletoMayusculas}`);
-alert(`Tu nombre completo es: ${nombreCompletoMayusculas}`);
-
-*/
-
-/*
---- Cuarto intento ---
-Se creó una lista map() para aplicar la conversión de mayúsculas a minúsculas a cada nombre, y se unieron los nombres con join() para convertirlos en la cadena que muestre el nombre completo en mayúsculas y así admitir el uso de la Ñ
-
-
-function ingresarDatos(mensaje) {
-  const dato = prompt(mensaje);
-  console.log(dato);
-  return dato;
-}
-
-const primerNombre = ingresarDatos("Ingresa tu primer nombre");
-const segundoNombre = ingresarDatos("Ingresa tu segundo nombre");
-const apellidoPaterno = ingresarDatos("Ingresa tu apellido paterno");
-const apellidoMaterno = ingresarDatos("Ingresa tu apellido materno");
-
-const nombres = [primerNombre, segundoNombre];
-const nombreCompleto = [...nombres, apellidoPaterno, apellidoMaterno]
-  .map((nombre) => nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase())
-  .join(" ");
-
-const nombreCompletoMayusculas = nombreCompleto.toLocaleUpperCase();
-
-console.log(`Tu nombre completo es: ${nombreCompletoMayusculas}`);
-alert(`Tu nombre completo es: ${nombreCompletoMayusculas}`);
-
---------------------------------------------------------------------
-
- Último intento: Se aplicó un while para validar los datos
-
-// Función para ingresar datos
-function ingresarDatos(mensaje) {
-    const dato = prompt(mensaje);
-    console.log(dato);
-    return dato;
-}
-
-let datosCorrectos = false;
-let primerNombre, segundoNombre, apellidoPaterno, apellidoMaterno;
-
-while (!datosCorrectos) {
-    // Pedir los nombres y apellidos y aplicar formato para que solo la primer letra de cada ingreso esté en mayúscula
-    primerNombre = ingresarDatos("Ingresa tu primer nombre");
-    const primeraLetraMayusculaPrimerNombre = primerNombre.charAt(0).toUpperCase();
-    const restoPrimerNombre = primerNombre.slice(1).toLowerCase();
-    primerNombre = primeraLetraMayusculaPrimerNombre + restoPrimerNombre;
-
-    segundoNombre = ingresarDatos("Ingresa tu segundo nombre");
-    const primeraLetraMayusculaSegundoNombre = segundoNombre.charAt(0).toUpperCase();
-    const restoSegundoNombre = segundoNombre.slice(1).toLowerCase();
-    segundoNombre = primeraLetraMayusculaSegundoNombre + restoSegundoNombre;
-
-    apellidoPaterno = ingresarDatos("Ingresa tu apellido paterno");
-    const primeraLetraMayusculaApellidoPaterno = apellidoPaterno.charAt(0).toUpperCase();
-    const restoApellidoPaterno = apellidoPaterno.slice(1).toLowerCase();
-    apellidoPaterno = primeraLetraMayusculaApellidoPaterno + restoApellidoPaterno;
-
-    apellidoMaterno = ingresarDatos("Ingresa tu apellido materno");
-    const primeraLetraMayusculaApellidoMaterno = apellidoMaterno.charAt(0).toUpperCase();
-    const restoApellidoMaterno = apellidoMaterno.slice(1).toLowerCase();
-    apellidoMaterno = primeraLetraMayusculaApellidoMaterno + restoApellidoMaterno;
-
-    // Mostrar los datos ingresados
-    const datosIngresados = `Por favor, revisa que los datos ingresados sean correctos:\nTu primer nombre es: ${primerNombre}\nTu segundo nombre es: ${segundoNombre}\nTu apellido paterno es: ${apellidoPaterno}\nTu apellido materno es: ${apellidoMaterno}`;
-    alert(datosIngresados);
-
-    const confirmacion = confirm("¿Los datos ingresados son correctos?");
-
-    if (confirmacion) {
-        datosCorrectos = true;
+while (!fechaValida) {
+    fechaNacimiento = prompt("Ingrese la fecha de nacimiento (dd/mm/aaaa): ");
+    if (/^(\d{2})\/(\d{2})\/(\d{4})$/.test(fechaNacimiento)) {
+        // se divide la fecha con split para calcular su edad
+        let partesFecha = fechaNacimiento.split("/");
+        let fechaNac = new Date(partesFecha[2], partesFecha[1] - 1, partesFecha[0]);
+        edad = calcularEdad(fechaNac);
+        fechaValida = true;
+    } else {
+        alert("El formato de la fecha es inválido, debe ser (dd/mm/aaaa)");
     }
 }
 
-// Formatear el nombre completo
-const nombreCompleto = `${primerNombre} ${segundoNombre} ${apellidoPaterno} ${apellidoMaterno}`;
+console.log("Fecha de nacimiento del alumno:", fechaNacimiento);
 
-// Mostrar el nombre completo con el formato aplicado usando templete literals
-const nombreCompletoMensaje = `Se ha registrado tu nombre completo: "${nombreCompleto}" en nuestra base de datos, gracias por registrarte.`;
-console.log(nombreCompletoMensaje);
-alert(nombreCompletoMensaje);
+// Función para calcular la edad usando objeto Math
+function calcularEdad(fechaNac) {
+    let diff_ms = Date.now() - fechaNac.getTime();
+    let age_dt = new Date(diff_ms);
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
 
- */
+console.log("Edad del alumno:", edad);
+
+// Capturar nacionalidad del alumno
+nacionalidad = prompt("Ingrese la nacionalidad del alumno: ");
+
+console.log("Nacionalidad del alumno:", nacionalidad);
+
+// Capturar correo electrónico del alumno
+let correoValido = false;
+while (!correoValido) {
+    correo = prompt("Ingrese el correo electrónico del alumno: ");
+    // Validar que el formato sea válido (usuario, @ y dominio)
+    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(correo)) {
+        correoValido = true;
+    } else {
+        alert("El correo electrónico ingresado no es válido.");
+    }
+}
+
+console.log("Correo electrónico del alumno:", correo);
+
+// Capturar teléfono del alumno validando que sean solo números
+let telefonoValido = false;
+while (!telefonoValido) {
+    telefono = prompt("Ingrese el número de teléfono del alumno (solo los números): ");
+    if (!isNaN(telefono)) {
+        telefonoValido = true;
+    } else {
+        alert("El número de teléfono ingresado no es válido.");
+    }
+}
+
+console.log("Teléfono del alumno:", telefono);
+
+// Capturar dirección del alumno
+direccion = prompt("Ingrese la dirección del alumno: ");
+
+// Capturar ciudad del alumno
+ciudad = prompt("Ingrese la ciudad del alumno: ");
+
+// Capturar estado del alumno
+estado = prompt("Ingrese el estado del alumno: ");
+
+// Capturar país del alumno
+pais = prompt("Ingrese el país del alumno: ");
+
+// Capturar código postal del alumno, validando que solo sean números
+let codigoPostalValido = false;
+while (!codigoPostalValido) {
+    codigoPostal = prompt("Ingrese el código postal del alumno: ");
+    if (!isNaN(codigoPostal)) {
+        codigoPostalValido = true;
+    } else {
+        alert("El código postal ingresado no es válido.");
+    }
+}
+
+console.log("Dirección del alumno:", direccion, ciudad, codigoPostal, estado, pais);
+
+// Capturar fecha de inscripción del alumno
+fechaInscripcion = new Date();
+
+console.log("Fecha de inscripción del alumno:", fechaInscripcion.toLocaleDateString());
+
+//Array de los niveles a los que puede inscribirse
+let niveles = ["Nido & Comunidad Infantil", "Casa de Niños", "Taller 1 & 2"];
+
+// Usando el método join para mostrar los niveles en el navegador
+alert("Niveles disponibles:\n\n" + niveles.join("\n"));
+
+let nivelElegido = prompt("Ingrese el número del nivel al que desea inscribirse: ");
+let nivelInscripcion = niveles[nivelElegido - 1];
+alert(`El alumno se ha inscrito en el nivel ${nivelInscripcion}`);
+
+// Capturar fecha de inicio del diplomado
+fechaInicio = new Date();
+
+// Capturar fecha de fin del diplomado
+fechaFin = new Date();
+fechaFin.setMonth(fechaFin.getMonth() + 6);
+
+console.log("Fecha de inicio del diplomado:", fechaInicio.toLocaleDateString());
+
+console.log("Fecha de fin del diplomado:", fechaFin.toLocaleDateString());
+
+// Capturar costo del diplomado
+costo = 5000;
+
+console.log("Costo del diplomado:", costo);
+
+// Capturar pago del alumno
+let pagoValido = false;
+while (!pagoValido) {
+    pago = prompt("Ingrese el pago del alumno: ");
+    if (!isNaN(pago)) {
+        pagoValido = true;
+    } else {
+        alert("El pago ingresado no es válido.");
+    }
+}
+
+console.log("Pago del alumno:", pago);
+
+// Calcular el pago total del alumno
+pagoTotal = pago * 6;
+
+console.log("Pago total del alumno:", pagoTotal);
+
+// Calcular el pago restante del alumno
+pagoRestante = pagoTotal - pago;
+
+console.log("Pago restante del alumno:", pagoRestante);
+
+// Mostrar los datos del alumno en el navegador
+alert(`Nombre del alumno: ${nombres}
+Fecha de nacimiento: ${fechaNacimiento}
+Edad: ${edad}
+Nacionalidad: ${nacionalidad}
+Correo electrónico: ${correo}
+Teléfono: ${telefono}
+Dirección: ${direccion}, ${ciudad}, ${codigoPostal}, ${estado}, ${pais}
+Fecha de inscripción: ${fechaInscripcion.toLocaleDateString()}
+Nivel de inscripción: ${nivelInscripcion}
+Fecha de inicio del diplomado: ${fechaInicio.toLocaleDateString()}
+Fecha de fin del diplomado: ${fechaFin.toLocaleDateString()}
+Costo del diplomado: ${costo}
+Pago del alumno: ${pago}
+Pago total del alumno: ${pagoTotal}
+Pago restante del alumno: ${pagoRestante}`);
+
+// Mostrar los datos del alumno en la consola
+console.log(`Nombre del alumno: ${nombres}
+Fecha de nacimiento: ${fechaNacimiento}
+Edad: ${edad}
+Nacionalidad: ${nacionalidad}
+Correo electrónico: ${correo}
+Teléfono: ${telefono}
+Dirección: ${direccion}, ${ciudad}, ${codigoPostal}, ${estado}, ${pais}
+Fecha de inscripción: ${fechaInscripcion.toLocaleDateString()}
+Nivel de inscripción: ${nivelInscripcion}
+Fecha de inicio del diplomado: ${fechaInicio.toLocaleDateString()}
+Fecha de fin del diplomado: ${fechaFin.toLocaleDateString()}
+Costo del diplomado: ${costo}
+Pago del alumno: ${pago}
+Pago total del alumno: ${pagoTotal}
+Pago restante del alumno: ${pagoRestante}`);
+
+// Dar la bienvenida al diplomado al alumno y dirigirlo a la página para que pueda empezar a tomarlo
+alert(`${nombres},bienvenido al diplomado para certificarte como Guía Montessori en el nivel ${nivelInscripcion} otorgado por la Asociación Montessori de México`);
+window.location.href = "https://www.certificacionmontessori.com";
